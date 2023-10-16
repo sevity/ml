bc7ff29d4fcf4bbb865fb5cc29a6627ff423b1f0
sevity <sevity@ymail.com>
2023-10-16 15:04:47 +0900

    random forest와 xgboost를 추가하고 성능비교

--- decision_tree/test/test.py
+++ decision_tree/test/test.py
@@ -1,22 +1,36 @@
 from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import xgboost as xgb
 
 # 데이터 로딩
 data = load_wine()
 X, y = data.data, data.target
 
-from sklearn.model_selection import train_test_split
-
 # 데이터 분할
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-
-# 모델 생성 및 학습
+# 디시전 트리 모델 생성 및 학습
 dt = DecisionTreeClassifier()
 dt.fit(X_train, y_train)
 
-# 평가
-y_pred = dt.predict(X_test)
-print(f'Accuracy: {accuracy_score(y_test, y_pred)}')
+# 랜덤 포레스트 모델 생성 및 학습
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+
+# XGBoost 모델 생성 및 학습
+xg_cls = xgb.XGBClassifier()
+xg_cls.fit(X_train, y_train)
+
+# 모델 평가
+y_pred_dt = dt.predict(X_test)
+print(f'Decision Tree Accuracy: {accuracy_score(y_test, y_pred_dt)}')
+
+y_pred_rf = rf.predict(X_test)
+print(f'Random Forest Accuracy: {accuracy_score(y_test, y_pred_rf)}')
+
+y_pred_xgb = xg_cls.predict(X_test)
+print(f'XGBoost Accuracy: {accuracy_score(y_test, y_pred_xgb)}')
 
